7c19100d3076063373a4a7d24aa7f6158a25fa6f
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-02 10:53:09 -0500

    processing extracted images

--- ImageDataGenerator.py
+++ ImageDataGenerator.py
@@ -5,3 +5,20 @@ local_zip = './horse-or-human.zip'
 zip_ref = zipfile.ZipFile(local_zip, 'r')
 zip_ref.extractall('./horse-or-human')
 zip_ref.close()
+
+import os
+
+# directory with training horse pics
+train_horse_dir = os.path.join('./horse-or-human/horses')
+
+# directory with training human pics
+train_human_dir = os.path.join('./horse-or-human/humans')
+
+train_horse_names = os.listdir(train_horse_dir)
+print(train_horse_names[:10])
+
+train_human_names = os.listdir(train_human_dir)
+print(train_human_names[:10])
+
+print('total training horse images:', len(os.listdir(train_horse_dir)))
+print('total training human images:', len(os.listdir(train_human_dir)))
