339f06d0b785ba9f35d2eba9d5ed925cb11e95c4
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-03 02:35:12 -0500

    get img dimensions and map pixel value

--- handling-complex-images.py
+++ handling-complex-images.py
@@ -16,3 +16,15 @@ plt.show()
 print("\nSample sad image:")
 plt.imshow(load_img(f"{os.path.join(sad_dir, os.listdir(sad_dir)[0])}"))
 plt.show()
+
+from tensorflow.keras.preprocessing.image import img_to_array
+
+sample_image = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
+sample_array = img_to_array(sample_image)
+print(f"Each image has shape: {sample_array.shape}")
+print(f"The maximum pixel value used is: {np.max(sample_array)}")
+
+"""
+Each image has shape: (150, 150, 3)
+The maximum pixel value used is: 255.0
+"""
