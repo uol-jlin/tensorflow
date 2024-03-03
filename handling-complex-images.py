9d0c635f488a4f2ad19c5a7339ae02fe78fa3816
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-03 12:31:31 -0500

    add batch data generator using ImageDataGenerator

--- handling-complex-images.py
+++ handling-complex-images.py
@@ -34,3 +34,18 @@ class myCallback(tf.keras.callbacks.Callback):
     if logs.get('accuracy') is not None and logs.get('accuracy') > 0.999:
       print("\nReached 99.9% accuracy so cancelling training!")
       self.model.stop_training = True
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def image_generator():
+  train_datagen = ImageDataGenerator(rescale=1/255)
+  # generate batches of augmented data
+    # - directory: relative path to the directory containing the data
+    # - target_size: equal to the resolution of each image (excluding the color dimension)
+    # - batch_size: number of images the generator yields when asked for a next batch. 
+    # - class_mode: How the labels are represented. Should be one of "binary", "categorical" or "sparse".
+  train_generator = train_datagen.flow_from_directory(directory='./data',
+                                                      target_size=(150,150),
+                                                      batch_size=10,
+                                                      class_mode='binary')
+  return train_generator
