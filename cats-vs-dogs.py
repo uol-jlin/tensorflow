61ee38442d6782d632a86eace534b2253c0b83cc
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-05 12:12:17 -0500

    download and extract dataset

---
+++ cats-vs-dogs.py
@@ -0,0 +1,30 @@
+import os
+import zipfile
+import random
+import shutil
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+import matplotlib.pyplot as plt
+
+!wget --no-check-certificate \
+    "https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip" \
+    -O "/tmp/cats-and-dogs.zip"
+
+local_zip = '/tmp/cats-and-dogs.zip'
+zip_ref   = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+source_path = '/tmp/PetImages'
+
+source_path_dogs = os.path.join(source_path, 'Dog')
+source_path_cats = os.path.join(source_path, 'Cat')
+
+# Deletes all non-image files (there are two .db files bundled into the dataset)
+!find /tmp/PetImages/ -type f ! -name "*.jpg" -exec rm {} +
+
+# os.listdir returns a list containing all files under the given path
+print(f"There are {len(os.listdir(source_path_dogs))} images of dogs.")
+print(f"There are {len(os.listdir(source_path_cats))} images of cats.")
+  
