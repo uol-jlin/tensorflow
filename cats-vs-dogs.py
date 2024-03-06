86e55cb69ccf1185fbfbf5104dedc74b12b3ed2c
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-06 16:02:50 -0500

    create Keras Sequential model

--- cats-vs-dogs.py
+++ cats-vs-dogs.py
@@ -112,17 +112,42 @@ def train_val_generators(TRAINING_DIR, VALIDATION_DIR):
     train_generator, validation_generator - tuple containing the generators
   """
 
-  train_datagen = ImageDataGenerator(rescale=1./255)  
+  train_datagen = ImageDataGenerator(rescale=1./255.0)  
 
   train_generator = train_datagen.flow_from_directory(directory=TRAINING_DIR,
                                                       batch_size=10,
                                                       class_mode='binary',
                                                       target_size=(150, 150))
 
-  validation_datagen = ImageDataGenerator(rescale=1./255) 
+  validation_datagen = ImageDataGenerator(rescale=1./255.0) 
 
   validation_generator = validation_datagen.flow_from_directory(directory=VALIDATION_DIR,
                                                                 batch_size=10,
                                                                 class_mode='binary',
                                                                 target_size=(150, 150))
   return train_generator, validation_generator
+
+def create_model():
+  model = tf.keras.models.Sequential([
+      tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+      tf.keras.layers.MaxPooling2D(2,2),
+
+      tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+
+      tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+
+      tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(512, activation='relu'),
+      tf.keras.layers.Dense(1, activation='sigmoid')
+  ])
+
+  model.compile(optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.001),
+                loss='binary_crossentropy',
+                metrics=['accuracy']) 
+
+  return model
