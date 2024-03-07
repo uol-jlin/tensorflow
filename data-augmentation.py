a6fee8dd5c59615758e5e5c2103d9314ab1bef1b
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-06 19:37:08 -0500

    download dataset from source

---
+++ data-augmentation.py
@@ -0,0 +1,18 @@
+import os
+import zipfile
+import random
+import shutil
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+import matplotlib.pyplot as plt
+
+!wget --no-check-certificate \
+    "https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip" \
+    -O "/tmp/cats-and-dogs.zip"
+
+local_zip = '/tmp/cats-and-dogs.zip'
+zip_ref   = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
