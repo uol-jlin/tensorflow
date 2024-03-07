fa57b88b6b6c1ec51c95d353e09989cb91b4e575
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-07 15:43:21 -0500

    comment out layer.weights explanation in keras layers

--- keras-layers.py
+++ keras-layers.py
@@ -4,8 +4,11 @@ from keras import layers
 layer = layers.Dense(32, activation='relu')
 inputs = keras.random.uniform(shape=(10, 20))
 outputs = layer(inputs)
+
+"""
 Unlike a function, though, layers maintain a state, updated when the layer receives data during training, and stored in layer.weights:
 
 >>> layer.weights
 [<KerasVariable shape=(20, 32), dtype=float32, path=dense/kernel>,
  <KerasVariable shape=(32,), dtype=float32, path=dense/bias>]
+ """
