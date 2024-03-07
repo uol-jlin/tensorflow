5202bff92157a7d31c95c86e4c86a3a4c4d96357
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-07 00:14:26 -0500

    define model architecture

--- data-augmentation.py
+++ data-augmentation.py
@@ -124,4 +124,24 @@ def train_val_generators(TRAINING_DIR, VALIDATION_DIR):
                                                                 target_size=(150, 150))
     
   return train_generator, validation_generator
+
+def create_model():
+
+  model = tf.keras.models.Sequential([ 
+      tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150,150,3)),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),    
+      tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(512, activation='relu'),
+      tf.keras.layers.Dense(1, activation='sigmoid'),
+  ])
+
+  model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001),
+                loss='binary_crossentropy',
+                metrics=['accuracy']) 
+    
+  return model
     
