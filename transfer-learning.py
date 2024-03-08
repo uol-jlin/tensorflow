544c07f1aebd2cbe0617f23bb65b001195b5538f
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-08 00:01:47 -0500

    flow training and validation images in batches

--- transfer-learning.py
+++ transfer-learning.py
@@ -93,3 +93,15 @@ train_datagen = ImageDataGenerator(rescale = 1./255.,
 
 # Validation data should not be augmented
 test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size = 20,
+                                                    class_mode = 'binary', 
+                                                    target_size = (150, 150))     
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator =  test_datagen.flow_from_directory( validation_dir,
+                                                          batch_size  = 20,
+                                                          class_mode  = 'binary', 
+                                                          target_size = (150, 150))
