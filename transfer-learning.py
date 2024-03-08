1081cb2cbd11c2363a39b01cdd2395a9366f9e30
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-07 23:48:59 -0500

    add dense layers to classifier

--- transfer-learning.py
+++ transfer-learning.py
@@ -28,3 +28,21 @@ pre_trained_model.summary()
 last_layer = pre_trained_model.get_layer('mixed7')
 print('last layer output shape: ', last_layer.output_shape)
 last_output = last_layer.output
+
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras import Model
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(last_output)
+# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Dense(1024, activation='relu')(x)
+# Add a dropout rate of 0.2
+x = layers.Dropout(0.2)(x)                  
+# Add a final sigmoid layer for classification
+x = layers.Dense  (1, activation='sigmoid')(x)           
+
+# Append the dense network to the base model
+model = Model(pre_trained_model.input, x) 
+
+# Print the model summary.
+model.summary()
