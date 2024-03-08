5dc92722609b73376bcdc653a4bd2473bc895da4
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-07 23:44:05 -0500

    set up pretrained model and freeze layer weights

--- transfer-learning.py
+++ transfer-learning.py
@@ -1,1 +1,24 @@
+# Download the pre-trained weights. No top means it excludes the fully connected layer it uses for classification.
+!wget --no-check-certificate \
+    https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5 \
+    -O /tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5
 
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras import layers
+
+# Set the weights file you downloaded into a variable
+local_weights_file = '/tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
+# Initialize the base model
+# Set the input shape and remove the dense layers
+pre_trained_model = InceptionV3(input_shape = (150, 150, 3), 
+                                include_top = False, 
+                                weights = None)
+
+# Load the pre-trained weights
+pre_trained_model.load_weights(local_weights_file)
+
+# Freeze the weights of the layers
+for layer in pre_trained_model.layers:
+  layer.trainable = False
+  
