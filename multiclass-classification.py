b8ad7a40463d5125a4f0f32c45bc0940431c9419
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-08 19:00:42 -0500

    download and extract zip file of dataset

--- multiclass-classification.py
+++ multiclass-classification.py
@@ -1,1 +1,19 @@
+# Download the train set
+!wget https://storage.googleapis.com/tensorflow-1-public/course2/week4/rps.zip
+    
+# Download the test set
+!wget https://storage.googleapis.com/tensorflow-1-public/course2/week4/rps-test-set.zip
+
+import zipfile
+
+# Extract the archive
+local_zip = './rps.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('tmp/rps-train')
+zip_ref.close()
+
+local_zip = './rps-test-set.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('tmp/rps-test')
+zip_ref.close()
 
