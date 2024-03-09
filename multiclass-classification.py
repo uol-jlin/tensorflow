75324ffae43be18a57dc5506e85933aeceec20d2
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-08 19:15:16 -0500

    prepare the ImageDataGenerator

--- multiclass-classification.py
+++ multiclass-classification.py
@@ -60,3 +60,33 @@ model.summary()
 
 # Set the training parameters
 model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+TRAINING_DIR = "tmp/rps-train/rps"
+training_datagen = ImageDataGenerator(
+      rescale = 1./255,
+	    rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+VALIDATION_DIR = "tmp/rps-test/rps-test-set"
+validation_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_generator = training_datagen.flow_from_directory(
+	TRAINING_DIR,
+	target_size=(150,150),
+	class_mode='categorical',
+  batch_size=126
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+	VALIDATION_DIR,
+	target_size=(150,150),
+	class_mode='categorical',
+  batch_size=126
+)
