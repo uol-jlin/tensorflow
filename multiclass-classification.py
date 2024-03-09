c66b008e91ca59867b0c5ade2b2cf1c0c6f9bdde
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-08 19:02:43 -0500

    sanity check on filenames

--- multiclass-classification.py
+++ multiclass-classification.py
@@ -17,3 +17,23 @@ zip_ref = zipfile.ZipFile(local_zip, 'r')
 zip_ref.extractall('tmp/rps-test')
 zip_ref.close()
 
+import os
+
+base_dir = 'tmp/rps-train/rps'
+
+rock_dir = os.path.join(base_dir, 'rock')
+paper_dir = os.path.join(base_dir, 'paper')
+scissors_dir = os.path.join(base_dir, 'scissors')
+
+print('total training rock images:', len(os.listdir(rock_dir)))
+print('total training paper images:', len(os.listdir(paper_dir)))
+print('total training scissors images:', len(os.listdir(scissors_dir)))
+
+rock_files = os.listdir(rock_dir)
+print(rock_files[:10])
+
+paper_files = os.listdir(paper_dir)
+print(paper_files[:10])
+
+scissors_files = os.listdir(scissors_dir)
+print(scissors_files[:10])
