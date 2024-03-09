6c778e519c2e6167d46fa334717ed12360e55bbe
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-09 15:51:22 -0500

    add data generator with image augmentation

--- csv-multiclass-classification.py
+++ csv-multiclass-classification.py
@@ -43,4 +43,42 @@ def parse_data_from_input(filename):
     images = np.array(images).astype('float')
 
     return images, labels
+
+def train_val_generators(training_images, training_labels, validation_images, validation_labels):
+  """
+  Creates the training and validation data generators
+  
+  Args:
+    training_images (array): parsed images from the train CSV file
+    training_labels (array): parsed labels from the train CSV file
+    validation_images (array): parsed images from the test CSV file
+    validation_labels (array): parsed labels from the test CSV file
     
+  Returns:
+    train_generator, validation_generator - tuple containing the generators
+  """
+  
+  training_images = np.expand_dims(training_images, axis=-1)
+  validation_images = np.expand_dims(validation_images, axis=-1)
+
+  train_datagen = ImageDataGenerator(rescale=1./255,
+                                     rotation_range=40,
+                                     width_shift_range=0.2,
+                                     height_shift_range=0.2,
+                                     shear_range=0.2,
+                                     zoom_range=0.2,
+                                     horizontal_flip=True,
+                                     fill_mode="nearest")
+
+  train_generator = train_datagen.flow(x=training_images,
+                                       y=training_labels,
+                                       batch_size=32) 
+
+  validation_datagen = ImageDataGenerator(rescale=1./255)
+
+  validation_generator = validation_datagen.flow(x=validation_images,
+                                                 y=validation_labels,
+                                                 batch_size=32) 
+
+  return train_generator, validation_generator
+  
