55a4ec599d869551186ac93aa1789050c49c4fc0
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-08 19:11:25 -0500

    define model architecture with convolution layers

--- multiclass-classification.py
+++ multiclass-classification.py
@@ -37,3 +37,23 @@ print(paper_files[:10])
 
 scissors_files = os.listdir(scissors_dir)
 print(scissors_files[:10])
+
+import tensorflow as tf
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+])
+
+# Print the model summary
+model.summary()
