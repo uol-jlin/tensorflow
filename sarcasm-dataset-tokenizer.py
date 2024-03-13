192116e6bc4be52800d36599d9276bc0f00070a0
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-13 01:01:59 -0400

    preprocess headlines

--- sarcasm-dataset-tokenizer.py
+++ sarcasm-dataset-tokenizer.py
@@ -19,3 +19,33 @@ for item in datastore:
     sentences.append(item['headline'])
     labels.append(item['is_sarcastic'])
     urls.append(item['article_link'])
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Initialize the Tokenizer class
+tokenizer = Tokenizer(oov_token="<OOV>")
+
+# Generate the word index dictionary
+tokenizer.fit_on_texts(sentences)
+
+# Print the length of the word index
+word_index = tokenizer.word_index
+print(f'number of words in word_index: {len(word_index)}')
+
+# Print the word index
+print(f'word_index: {word_index}')
+print()
+
+# Generate and pad the sequences
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding='post')
+
+# Print a sample headline
+index = 2
+print(f'sample headline: {sentences[index]}')
+print(f'padded sequence: {padded[index]}')
+print()
+
+# Print dimensions of padded sequences
+print(f'shape of padded sequences: {padded.shape}')
