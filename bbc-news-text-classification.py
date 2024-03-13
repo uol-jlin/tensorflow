1389e9a16ec31c95dc0561dcb0df7a05514c71e8
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-13 19:58:25 -0400

    inspect structure of csv file

--- bbc-news-text-classification.py
+++ bbc-news-text-classification.py
@@ -1,1 +1,7 @@
+import csv
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+with open("./data/bbc-text.csv", 'r') as csvfile:
+    print(f"First line (header) looks like this:\n\n{csvfile.readline()}")
+    print(f"Each data point looks like this:\n\n{csvfile.readline()}")     
