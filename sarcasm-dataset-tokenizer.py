738a500c567882fa327f2022cfe65d1148b3d807
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-13 00:54:54 -0400

    load and inspect sarcasm dataset

---
+++ sarcasm-dataset-tokenizer.py
@@ -0,0 +1,25 @@
+import json
+
+with open("./sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+# Non-sarcastic headline
+print(datastore[0])
+
+# Sarcastic headline
+print(datastore[20000])
+
+sentences = []
+labels = []
+urls = []
+
+# Initialize lists
+sentences = [] 
+labels = []
+urls = []
+
+# Append elements in the dictionaries into each list
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
