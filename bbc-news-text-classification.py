59aba0f80c3b130628b15b9d300cbad110102a70
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-13 20:04:00 -0400

    helper func to remove stopwords from sentence

--- bbc-news-text-classification.py
+++ bbc-news-text-classification.py
@@ -5,3 +5,22 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 with open("./data/bbc-text.csv", 'r') as csvfile:
     print(f"First line (header) looks like this:\n\n{csvfile.readline()}")
     print(f"Each data point looks like this:\n\n{csvfile.readline()}")     
+
+def remove_stopwords(sentence):
+    """
+    Removes a list of stopwords
+    
+    Args:
+        sentence (string): sentence to remove the stopwords from
+    
+    Returns:
+        sentence (string): lowercase sentence without the stopwords
+    """
+    stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+    
+    sentence = sentence.lower()
+    split_sentence = sentence.split(" ")
+    cleaned_sentence = [word for word in split_sentence if word not in stopwords]
+    
+    return " ".join(cleaned_sentence)
+    
