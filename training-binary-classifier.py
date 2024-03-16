99f703b1fa30df3938a925b8a1f03d5c3d114026
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-16 12:36:42 -0400

    split dataset

--- training-binary-classifier.py
+++ training-binary-classifier.py
@@ -4,3 +4,24 @@ imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
 
 for example in imdb['train'].take(2):
   print(example)
+
+import numpy as np
+
+train_data, test_data = imdb['train'], imdb['test']
+
+training_sentences = []
+training_labels = []
+
+testing_sentences = []
+testing_labels = []
+
+for s,l in train_data:
+  training_sentences.append(s.numpy().decode('utf8'))
+  training_labels.append(l.numpy())
+
+for s,l in test_data:
+  testing_sentences.append(s.numpy().decode('utf8'))
+  testing_labels.append(l.numpy())
+
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
