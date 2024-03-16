47a415353aaadb7bf4e0119faa7cbc72362df7b1
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-16 12:55:57 -0400

    generate padded sequences

--- training-binary-classifier.py
+++ training-binary-classifier.py
@@ -25,3 +25,23 @@ for s,l in test_data:
 
 training_labels_final = np.array(training_labels)
 testing_labels_final = np.array(testing_labels)
+
+vocab_size = 10000
+max_length = 120
+embedding_dim = 16
+trunc_type='post'
+oov_tok = "<OOV>"
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded = pad_sequences(sequences,maxlen=max_length, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences,maxlen=max_length, truncating=trunc_type)
