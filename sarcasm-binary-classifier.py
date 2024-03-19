4535cc723dd6a140f00cb6419acfffcbd3c3706d
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-19 00:41:55 -0400

    add hyperparameters

--- sarcasm-binary-classifier.py
+++ sarcasm-binary-classifier.py
@@ -9,3 +9,15 @@ labels = []
 for item in datastore:
     sentences.append(item['headline'])
     labels.append(item['is_sarcastic'])
+
+# Number of examples to use for training
+training_size = 20000
+
+# Vocabulary size of the tokenizer
+vocab_size = 10000
+
+# Maximum length of the padded sequences
+max_length = 32
+
+# Output dimensions of the Embedding layer
+embedding_dim = 16
