6de54855884124cf6e1f852b74c581e796ef5e04
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-19 00:41:16 -0400

    load json file

--- sarcasm-binary-classifier.py
+++ sarcasm-binary-classifier.py
@@ -1,1 +1,11 @@
+import json
 
+with open("./sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
