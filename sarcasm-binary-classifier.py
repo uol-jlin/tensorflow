5067ddb0a3d3cbc7d44ad9de8e9dfb41f7ac14a7
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-20 00:11:11 -0400

    preprocess train and test sets

--- sarcasm-binary-classifier.py
+++ sarcasm-binary-classifier.py
@@ -29,3 +29,31 @@ testing_sentences = sentences[training_size:]
 # Split the labels
 training_labels = labels[0:training_size]
 testing_labels = labels[training_size:]
+
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Parameters for padding and OOV tokens
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+
+# Initialize the Tokenizer class
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+
+# Generate the word index dictionary
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+
+# Generate and pad the training sequences
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+# Generate and pad the testing sequences
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+# Convert the labels lists into numpy arrays
+training_labels = np.array(training_labels)
+testing_labels = np.array(testing_labels)
