9ecd3b993ba71d73c129f22561d620bf6856ebd4
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-21 19:41:42 -0400

    load IMDB plain text and pre-tokenized datasets

--- subword-tokenization.py
+++ subword-tokenization.py
@@ -1,1 +1,7 @@
+import tensorflow_datasets as tfds
 
+# Download the plain text default config
+imdb_plaintext, info_plaintext = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+
+# Download the subword encoded pretokenized dataset
+imdb_subwords, info_subwords = tfds.load("imdb_reviews/subwords8k", with_info=True, as_supervised=True)
