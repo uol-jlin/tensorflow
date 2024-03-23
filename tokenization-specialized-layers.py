5e1fa3fa28558b1b21f5802eb2345d8e409dd7b2
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-23 00:15:25 -0400

    examine structure of bbc text csv file

--- tokenization-specialized-layers.py
+++ tokenization-specialized-layers.py
@@ -5,3 +5,8 @@ import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import matplotlib.pyplot as plt
+
+with open("./bbc-text.csv", 'r') as csvfile:
+    print(f"First line (header) looks like this:\n\n{csvfile.readline()}")
+    print(f"Each data point looks like this:\n\n{csvfile.readline()}")
+  
