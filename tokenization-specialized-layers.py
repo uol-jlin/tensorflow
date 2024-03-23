67a2ff2dfbeb31ae406e1f082167d0f56f59508b
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-23 00:14:34 -0400

    add imports

---
+++ tokenization-specialized-layers.py
@@ -0,0 +1,7 @@
+import io
+import csv
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
