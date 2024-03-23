15f2b89b9428d0e20275a7031fb073433c094785
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-23 16:12:52 -0400

    download subwords8k pre-tokenized IMDB review dataset

--- single-layer-LSTM.py
+++ single-layer-LSTM.py
@@ -1,1 +1,7 @@
-s
+import tensorflow_datasets as tfds
+
+# Download the subword encoded pretokenized dataset
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
+
+# Get the tokenizer
+tokenizer = info.features['text'].encoder
