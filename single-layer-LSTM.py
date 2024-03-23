65b21c416b299357537493f3ae86186d68cd90c0
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-23 16:30:54 -0400

    get train/test splits and generate padded batches

--- single-layer-LSTM.py
+++ single-layer-LSTM.py
@@ -5,3 +5,16 @@ dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervis
 
 # Get the tokenizer
 tokenizer = info.features['text'].encoder
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 256
+
+# Get the train and test splits
+train_data, test_data = dataset['train'], dataset['test'], 
+
+# Shuffle the training data
+train_dataset = train_data.shuffle(BUFFER_SIZE)
+
+# Batch and pad the datasets to the maximum length of the sequences
+train_dataset = train_dataset.padded_batch(BATCH_SIZE)
+test_dataset = test_data.padded_batch(BATCH_SIZE)
