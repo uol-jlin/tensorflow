00e9a5a702b25399616d5c75bb48ddd6a915c2a0
Jonathan <133916425+uol-jlin@users.noreply.github.com>
2024-03-23 00:28:17 -0400

    add methods to remove stopwords and parse data from file

--- tokenization-specialized-layers.py
+++ tokenization-specialized-layers.py
@@ -10,3 +10,48 @@ with open("./bbc-text.csv", 'r') as csvfile:
     print(f"First line (header) looks like this:\n\n{csvfile.readline()}")
     print(f"Each data point looks like this:\n\n{csvfile.readline()}")
   
+def remove_stopwords(sentence):
+    """
+    Removes a list of stopwords
+    
+    Args:
+        sentence (string): sentence to remove the stopwords from
+    
+    Returns:
+        sentence (string): lowercase sentence without the stopwords
+    """
+    # List of stopwords
+    stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+    
+    # Sentence converted to lowercase-only
+    sentence = sentence.lower()
+
+    words = sentence.split()
+    no_words = [w for w in words if w not in stopwords]
+    sentence = " ".join(no_words)
+
+    return sentence
+
+def parse_data_from_file(filename):
+    """
+    Extracts sentences and labels from a CSV file
+    
+    Args:
+        filename (string): path to the CSV file
+    
+    Returns:
+        sentences, labels (list of string, list of string): tuple containing lists of sentences and labels
+    """
+    sentences = []
+    labels = []
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            labels.append(row[0])
+            sentence = row[1]
+            sentence = remove_stopwords(sentence)
+            sentences.append(sentence)
+
+    return sentences, labels
+    
